8d935cf5ef8160f6932d23153aeeb0589ad6957d
b81984 <lordxchris@gmail.com>
2013-09-12 03:25:09 -0500

    Setting Functions and Arguments

--- Lohse_Nathan_assignment2.js
+++ Lohse_Nathan_assignment2.js
@@ -1,1 +1,29 @@
-alert("JavaScript works!");
+/*Nathan Lohse
+9/12/2013
+x-COM: Iron Man*/
+
+var sniperKill = false;
+var assaultLives = false;
+var assaultName = "Frankie";
+var commanderName = "Jax";
+var squaddies = 5;
+
+var function sniperShot(percentHit) //returns sniperKill boolean, 'procedure'
+{
+
+}
+
+var function assaultCharge(chargeOrShoot, sniperKill) //Boolean Function
+{
+
+}
+
+var function fallenNames(commanderName, assaultName) //String Function
+{
+
+}
+
+var function survivingSquad(squaddies, assaultLives) //Number Function
+{
+
+}
